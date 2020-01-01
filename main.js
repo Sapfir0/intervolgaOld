1e81eff34609d2fe4442aa2f3efb972c3a69b688
Sapfir0 <sapfir999999@yandex.ru>
2020-01-01 20:11:07 +0400

    Создал начальную структуру

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const addStudentBtn = document.getElementsByClassName("addStudentBtn")[0];
+    const studentName = document.getElementsByName("studentName");
+    const studentGroup = document.getElementsByName("studentGroup");
+    
+    addStudentBtn.addEventListener("click", async () => {
+        //TODO validate
+
+        const options = {
+            method:"post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                "studentName": studentName.value,
+                "studentGroup": studentGroup.value
+            })
+        };
+        let response = await fetch("/addStudent", options);
+    })
+
+});
